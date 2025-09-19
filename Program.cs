57ec14f350e865273ea6643fa37edfaa64c252fb
savadah <133503793+savadah@users.noreply.github.com>
2025-09-19 09:52:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public class State
+        {
+            protected string Name;
+            public State(string name) { Name = name; }
+            public void Print() { Console.WriteLine($"Государство: {Name}"); }
+        }
+
+        public class Monarchy : State
+        {
+            protected string RulerTitle;
+            protected string RulerName;
+            protected int KingdomsCount;
+
+            public Monarchy(int kingdomsCount, string rulerTitle, string rulerName, string name)
+                : base(name)
+            {
+                KingdomsCount = kingdomsCount;
+                RulerTitle = rulerTitle;
+                RulerName = rulerName;
+            }
+
+            public new void Print()
+            {
+                Console.WriteLine($"Монархия {Name}: правит {RulerTitle} {RulerName}, королевств в составе {KingdomsCount}");
+            }
+        }
+
+        public class Kingdom : Monarchy
+        {
+            private int ProvincesCount;
+            private int Population;
+
+            public Kingdom(int provincesCount, int population, int kingdomsCount, string rulerName, string name)
+                : base(kingdomsCount, "Король", rulerName, name)
+            {
+                ProvincesCount = provincesCount;
+                Population = population;
+            }
+
+            public new void Print()
+            {
+                Console.WriteLine($"Королевство {Name}: король {RulerName}, провинций {ProvincesCount}, население {Population}");
+            }
+        }
+
+        public class Republic : State
+        {
+            private string PresidentName;
+            private int ParliamentSeats;
+
+            public Republic(string presidentName, int parliamentSeats, string name)
+                : base(name)
+            {
+                PresidentName = presidentName;
+                ParliamentSeats = parliamentSeats;
+            }
+
+            public new void Print()
+            {
+                Console.WriteLine($"Республика {Name}: президент {PresidentName}, мест в парламенте {ParliamentSeats}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var k = new Kingdom(30, 12_000_000, 1, "Артур", "Авалон");
+            k.Print();
+
+            var m = new Monarchy(3, "Император", "Юстиниан", "Византия");
+            m.Print();
+
+            var r = new Republic("Сыров Кирилл", 100, "Новая Республика");
+            r.Print();
+
+            var s = new State("Геар");
+            s.Print();
+        }
+    }
+}
